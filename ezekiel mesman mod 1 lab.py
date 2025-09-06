fef2c51c8dbe4f8e60c524419fda0cafd8a6d015
Zorklstein <Zbro1252@gmail.com>
2025-09-06 12:39:07 -0400

    Add files via upload

---
+++ ezekiel mesman mod 1 lab.py
@@ -0,0 +1,26 @@
+'''ezekiel mesman mod 1 lab'''
+
+characterName = "spider man"
+characterAge = 14
+heroStatus = True
+
+
+
+if heroStatus == True:
+    print(f"{characterName} is a {characterAge} year old hero.")
+
+if heroStatus == False:
+    print(f"{characterName} is a {characterAge} year old villan.")
+
+characterAge = characterAge + 5
+heroStatus = False
+
+
+
+if heroStatus == True:
+    print(f"{characterName} is a {characterAge} year old hero.")
+
+if heroStatus == False:
+    print(f"{characterName} is a {characterAge} year old villan.")
+
+
